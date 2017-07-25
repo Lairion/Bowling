ab82028542a169e944dd2d47bcbb95323bef09c2
manul <Nunyes_m_a@ukr.net>
2017-07-25 20:55:41 +0300

    Added functional from input points

--- bowling.py
+++ bowling.py
@@ -1,12 +1,22 @@
 class Player:
-	def __init__(self,name):
-		self.name = name
-		self.points = []
+    def __init__(self,name):
+        self.name = name
+        self.points = []
 players = []
 players_count = int(input("How many players?"))
 for i in range(players_count):
-	players.append(Player(input("Input name player "+str(i+1))))
-string = ''
+    players.append(Player(input("Input name player "+str(i+1))))
+for i in range(3):
+    for player in players:
+        first_throw = int(input("First Throw: "))
+        if first_throw == 10:
+            player.points+=[[first_throw,0,"X"]]
+        else:
+            second_throw = int(input("Second Throw: "))
+            if first_throw+second_throw >= 10:
+                player.points+=[[first_throw,second_throw,"/"]]
+            else:
+               player.points+=[[first_throw,second_throw,first_throw+second_throw]]
+        print(player.name,player.points)
 for player in players:
-	print(player.name)
-	
+    print(player.name,player.points)
