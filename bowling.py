e998074d6af477e701b9e27464e1535e3600b509
manul <Nunyes_m_a@ukr.net>
2017-07-27 20:58:29 +0300

    Added last Frame

--- bowling.py
+++ bowling.py
@@ -2,12 +2,24 @@ class Player:
     def __init__(self,name):
         self.name = name
         self.points = []
+def check_simple_throw(first_throw):
+    if first_throw >= 10:
+        points = [[first_throw,0,"X"]]
+    else:
+        second_throw = int(input("Second Throw: "))
+        if first_throw+second_throw >= 10:
+            points = [[first_throw,second_throw,"/"]]
+        else:
+            points = [[first_throw,second_throw,first_throw+second_throw]]
+    return points
+
 players = []
-players_count = int(input("How many players?"))
+players_count = int(input("How many players? "))
 for i in range(players_count):
     players.append(Player(input("Input name player "+str(i+1))))
 for i in range(3):
     for player in players:
+        print(player.name)
         first_throw = int(input("First Throw: "))
         if player.points:
             if player.points[-1][2]  == "X":
@@ -16,6 +28,7 @@ for i in range(3):
                         player.points[-2][2] = 20 + first_throw
                 if first_throw >= 10:
                     player.points+=[[first_throw,0,"X"]]
+                    print(player.name,player.points)
                 else:
                     second_throw = int(input("Second Throw: "))
                     player.points[-1][2]  = 10 + first_throw + second_throw
@@ -24,37 +37,70 @@ for i in range(3):
                     else:
                         player.points+=[[first_throw,second_throw,first_throw+second_throw]]
                     print(player.name,player.points)
-            elif player.points[-1][2]  == "/" and player.points:
+            elif player.points[-1][2]  == "/":
                 player.points[-1][2] = 10 + first_throw 
-                if first_throw >=10:
-                    player.points+=[[first_throw,0,"X"]]
-                else:
-                    second_throw = int(input("Second Throw: "))
-                    if first_throw+second_throw >= 10:
-                        player.points+=[[first_throw,second_throw,"/"]]
-                    else:
-                       player.points+=[[first_throw,second_throw,first_throw+second_throw]]
-                    print(player.name,player.points)
+                player.points += check_simple_throw(first_throw)
+                print(player.name,player.points)
             else:
-                if first_throw == 10:
-                    player.points+=[[first_throw,0,"X"]]
-                else:
-                    second_throw = int(input("Second Throw: "))
-                    if first_throw+second_throw >= 10:
-                        player.points+=[[first_throw,second_throw,"/"]]
-                    else:
-                       player.points+=[[first_throw,second_throw,first_throw+second_throw]]
+                player.points += check_simple_throw(first_throw)
                 print(player.name,player.points)
 
         else:
-            if first_throw == 10:
-                player.points+=[[first_throw,0,"X"]]
-            else:
-                second_throw = int(input("Second Throw: "))
-                if first_throw+second_throw >= 10:
-                    player.points+=[[first_throw,second_throw,"/"]]
-                else:
-                   player.points+=[[first_throw,second_throw,first_throw+second_throw]]
+            player.points += check_simple_throw(first_throw)
             print(player.name,player.points)
+
+
 for player in players:
-    print(player.name,player.points)
+    first_throw = int(input("First Throw: "))
+    if player.points[-1][2]  == "X":
+        if len(player.points)>1:
+            if player.points[-2][2] == "X":
+                player.points[-2][2] = 20 + first_throw
+        if first_throw >= 10:
+            
+            second_throw = int(input("Second Throw: "))
+            player.points[-1][2]  = 10 + first_throw + second_throw
+            third_throw = int(input("Third Throw: "))
+            player.points+=[[first_throw,second_throw,third_throw,sum([first_throw,second_throw,third_throw])]]
+            print(player.name,player.points)
+        else:
+            second_throw = int(input("Second Throw: "))  
+            player.points[-1][2]  = 10 + first_throw + second_throw
+            if second_throw+first_throw >= 10:               
+                third_throw = int(input("Third Throw: "))
+                player.points+=[[first_throw,second_throw,third_throw,sum([first_throw,second_throw,third_throw])]]
+            else:
+                player.points+=[[first_throw,second_throw,0,first_throw+second_throw]]
+            print(player.name,player.points)
+    elif player.points[-1][2]  == "/":
+         player.points[-1][2] = 10 + first_throw 
+         if first_throw >= 10:
+            second_throw = int(input("Second Throw: "))
+            third_throw = int(input("Third Throw: "))
+            player.points+=[[first_throw,second_throw,third_throw,sum([first_throw,second_throw,third_throw])]]
+            print(player.name,player.points)
+         else:
+            second_throw = int(input("Second Throw: "))  
+            if second_throw+first_throw >= 10:               
+                third_throw = int(input("Third Throw: "))
+                player.points+=[[first_throw,second_throw,third_throw,sum([first_throw,second_throw,third_throw])]]
+            else:
+                player.points+=[[first_throw,second_throw,0,first_throw+second_throw]]
+            print(player.name,player.points)
+    else:
+        player.points += check_simple_throw(first_throw)
+        print(player.name,player.points)
+        if first_throw >= 10:
+            second_throw = int(input("Second Throw: "))
+            third_throw = int(input("Third Throw: "))
+            player.points+=[[first_throw,second_throw,third_throw,sum([first_throw,second_throw,third_throw])]]
+            print(player.name,player.points)
+        else:
+            second_throw = int(input("Second Throw: "))  
+            if second_throw+first_throw >= 10:               
+                third_throw = int(input("Third Throw: "))
+                player.points+=[[first_throw,second_throw,third_throw,sum([first_throw,second_throw,third_throw])]]
+            else:
+                player.points+=[[first_throw,second_throw,0,first_throw+second_throw]]
+            print(player.name,player.points)
+    
